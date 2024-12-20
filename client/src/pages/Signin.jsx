4db8c13ed6453f78d691a2de1da4a13b18b54f9f
Muteeb Haider <muteebhdr@gmail.com>
2024-12-20 19:50:13 +0500

    Signup page ui design

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from "react";
 
-export default function Signin() {
-  return (
-    <div>Signin</div>
-  )
+export default function SignIn() {
+  return <div>Signin</div>;
 }
